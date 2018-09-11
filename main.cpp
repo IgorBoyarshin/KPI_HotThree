c0ab41732cc00f1a149a18b539b33a4b27c811ac
Igor Boyarshin <igor.boyarshin@gmail.com>
2018-09-11 11:29:58 +0300

    yet amother loop added

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main() {
     for (unsigned int i = 0; i < 5; i++) {
         std::cout << i << std::endl;
     }
+    for (unsigned int i = 1; i < 5; i++) {
+        std::cout << i << std::endl;
+    }
 
     return 0;
 }
