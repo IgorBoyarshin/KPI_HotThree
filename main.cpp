92a14b88c3abf267b9cc905f626f8b188d00c947
Artem Berezynets <artem9825@gmail.com>
2018-09-11 11:45:15 +0300

    Revert "HAHAHA deleted everything. suck it"
    This reverts commit 4b74e228f613c63c2308e0194fab8086b1f70bae.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 
 int main() {
+    std::cout << "Hello there!" << std::endl;
+    std::cout << "New line" << std::endl;
+
+    for (unsigned int i = 0; i < 5; i++) {
+        std::cout << i << std::endl;
+        std::cout << (i+1) << std::endl;
+    }
+    for (unsigned int i = 1; i < 5; i++) {
+        std::cout << i << std::endl;
+    }
 
     return 0;
 }
