8c3ee54e1f2a2e0bde7b328b22e6b004e71b1d07
Igor Boyarshin <igor.boyarshin@gmail.com>
2018-09-11 11:12:37 +0300

    loop added

--- main.cpp
+++ main.cpp
@@ -6,5 +6,9 @@ int main() {
     std::cout << "Hello there!" << std::endl;
     std::cout << "New line" << std::endl;
 
+    for (unsigned int i = 0; i < count; i++) {
+        std::cout << i << std::endl;
+    }
+
     return 0;
 }
